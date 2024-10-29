22988bf4abd047e01b6e6b2a0c23f3313ccf80ea
egedemirtas <egexyz@gmail.com>
2024-10-29 21:03:13 +0300

    update config at runtime

--- cards/src/main/java/com/bank/app/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/bank/app/cards/dto/CardsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.bank.app.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,7 +9,18 @@ import java.util.Map;
 
 // IMPORTANT: as of java 16 we have "record", they act as carrier of immutable data
 // if you have java 15 or below, you should use final fields, all args constructor and only getter methods
+/*
 @ConfigurationProperties(prefix = "cards")
 public record CardsContactInfoDto(String message, Map<String, String> contactDetails,
                                   List<String> onCallSupport) {
+}
+*/
+
+@ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
